78d07d0093916b7cd945ee0f5c2ec2debec6ba26
OwaisAbdullah644 <anonymoushackerunknown10@gmail.com>
2025-10-27 17:46:45 +0500

    re-building

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SoulWhispers from './pages/SoulWhispers';
-import GymKey from './pages/GymKey';
-import Contact from './pages/Contact';
-import FloatingAssistant from './components/FloatingAssistant';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import About from './components/About';
-import AppLayout from './AppLayout';
-import Register from './pages/Register';
 import ComingSoon from './pages/ComingSoon';
-// import Ribbons from './components/Ribbons';
 
 const App = () => {
   return (
